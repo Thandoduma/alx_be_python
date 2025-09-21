d6b15ba6d00f94f6306e7f48be8c5044f1f72d81
noluthando duma <thandobi24@gmail.com>
2025-09-21 19:44:00 +0200

    feat: Add weather advice script

---
+++ python_introduction/practice.py
@@ -0,0 +1,15 @@
+from queue import Full
+
+
+print ("I like pizza!")
+print ("it's really good!")
+full_name = "Thando"
+
+print (f"hello {full_name}")
+age = 24 
+print (f"You are {age} years old")
+
+print(f"Are you {age} years old?") 
+
+if age < 30:
+    print("You are still young!") 
