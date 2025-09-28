5f2b05807e7d89ddbde7c6365aeeb00873c738c1
noluthando duma <thandobi24@gmail.com>
2025-09-28 20:13:08 +0200

    Fix division by zero check to match ALX checker requirements

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -24,10 +24,10 @@ def perform_operation(num1, num2, operation):
         return num1 * num2
     
     elif operation == 'divide':
-        if num2 != 0:
-            return num1 / num2
-        else:
+        if num2 == 0:
             return "Error: Cannot divide by zero."
+        else:
+            return num1 / num2
     
     else:
         return "Error: Invalid operation."
